82fabd26c05be93dc5e11f980d47978990d7bcfb
welligton-home <welligtoncos@gmail.com>
2023-04-02 19:21:29 -0300

    stepper sales

--- src/app/stepper/step.module.ts
+++ src/app/stepper/step.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PoModule } from '@po-ui/ng-components';
+import { PoModule, PoStepperModule, PoWidgetModule } from '@po-ui/ng-components';
 import { SamplePoStepperBasicComponent } from './step/sample-po-stepper-basic.component';
 import { StepperRoutingModule } from './step-routing.module';
+import { SamplePoStepperSalesComponent } from './sample-po-stepper-sales/sample-po-stepper-sales.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    SamplePoStepperBasicComponent
+    SamplePoStepperBasicComponent,
+    SamplePoStepperSalesComponent
   ],
   imports: [
     CommonModule,
     PoModule,
-    StepperRoutingModule
-
-  ]
+    StepperRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class StepperModule { }
